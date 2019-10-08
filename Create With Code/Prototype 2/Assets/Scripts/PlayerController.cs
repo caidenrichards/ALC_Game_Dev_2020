a2b3ef799855b7082eb05c13cf9d073506476bdd
Caiden Richards <caiden.r762@stu.nebo.edu>
2019-10-08 10:29:33 -0600

    Added player and player control

---
+++ Create With Code/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    public float horizontalInput;
+    public float verticalInput;
+    public float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(transform.position.x < -10)
+        {
+            transform.position = new Vector3(-10, transform.position.y, transform.position.z);
+        }
+
+        else if (transform.position.x > 10)
+        {
+            transform.position = new Vector3(10, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.z < -10)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -10);
+        }
+
+        else if (transform.position.z > 10)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, 10);
+        }
+
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(Vector3.up * Time.deltaTime * verticalInput * speed);
+
+    }
+}
