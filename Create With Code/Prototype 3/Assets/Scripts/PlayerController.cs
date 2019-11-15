6b87b500804870ae76272193113a9d69951b470e
Caiden Richards <caiden.r762@stu.nebo.edu>
2019-11-15 10:29:34 -0700

    Prototype Update 5

--- Create With Code/Prototype 3/Assets/Scripts/PlayerController.cs
+++ Create With Code/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -6,25 +6,31 @@ public class PlayerController : MonoBehaviour
 {
 
     private Rigidbody playerRb;
+    private Animator playerAnim;
+
     public float jumpForce;
     public float gravityModifier;
     public bool isOnGround = true;
     public bool gameOver;
 
+
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
 
     }
@@ -38,6 +44,8 @@ public class PlayerController : MonoBehaviour
         {
             Debug.Log("Game Over");
             gameOver = true;
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
         }
     }
 }
