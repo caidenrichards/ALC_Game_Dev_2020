014ecc5ed6cc0f784a7d3178a2a4de3efc0a5d04
Caiden Richards <caiden.r762@stu.nebo.edu>
2019-12-20 10:29:58 -0700

    Prototype 5 Update 5

--- Create With Code/Prototype 5/Assets/Scripts/Target.cs
+++ Create With Code/Prototype 5/Assets/Scripts/Target.cs
@@ -5,19 +5,54 @@ using UnityEngine;
 public class Target : MonoBehaviour
 {
 	private Rigidbody targetRb;
+
+    private float minSpeed = 12;
+    private float maxSpeed = 16;
+    private float maxTorque = 10;
+    private float xRange = 4;
+    private float ySpawnPos = -6;
+
+    public int pointValue;
+    public ParticleSystem explosionParticle;
+
+    private GameManager gameManager;
     // Start is called before the first frame update
     void Start()
     {
+        gameManager = GameObject.Find("Game Manager").GetComponent<GameManager>();
 		targetRb = GetComponent<Rigidbody>();
-		targetRb.AddForce(Vector3.up * Random.Range(12, 16), ForceMode.Impulse);
-        targetRb.AddTorque(Random.Range(-10, 10), Random.Range(-10, 10)),
-        Random.Range(-10, 10), ForceMode.Impulse):
-        transform.position = new Vector3(Random.Range(-4, 4), -6);
+		targetRb.AddForce(RandomForce(), ForceMode.Impulse);
+        targetRb.AddTorque(RandomTorque(), RandomTorque(), RandomTorque(), ForceMode.Impulse);
+        transform.position = RandomSpawnPos();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    Vector3 RandomForce()
+    {
+        return Vector3.up * Random.Range(minSpeed, maxSpeed);
+    }
+    float RandomTorque()
+    {
+        return Random.Range(-maxTorque, maxTorque);
+    }
+    Vector3 RandomSpawnPos()
+    {
+        return new Vector3(Random.Range(-xRange, xRange), ySpawnPos);
+    }
+
+    private void OnMouseDown()
+    {
+        Destroy(gameObject);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
     }
 }
